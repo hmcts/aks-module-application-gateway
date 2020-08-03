936232186f8f7168c3ee5bf0171d818114a12b84
stravze <stravze@gmail.com>
2020-08-03 11:30:44 +0100

    removed unused variables

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -1,27 +1,7 @@
-variable "app_gateway_name" {}
-
-variable "app_gateway_sku" {}
-
-variable "backend_address_pool_name" {}
-
-variable "frontend_ip_configuration_name" {}
-
-variable "frontend_port_name" {}
-
-variable "http_setting_name" {}
-
-variable "listener_name" {}
-
-variable "request_routing_rule_name" {}
-
 variable "resource_group_name" {}
-#variable "application_gateway_subnet" {}
-
 variable "deploy_environment" {}
-#variable "network_name" {}
-variable "network_shortname" {}
-#variable "network_resource_group_name" {}
 
+variable "network_shortname" {}
 variable "network_location" {}
 
 variable "tag_project_name" {}
